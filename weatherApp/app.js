84d9bc24ac902af4748db04f044ca6b03a13a173
=Deep.Chhatbar <=deepchhatbar@gmail.com>
2023-08-26 14:55:27 +0530

    user can enter address from commandline feature

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,30 +1,15 @@
-const request = require('request')
-const url = "http://api.weatherstack.com/current?access_key=dac86668ffe75e11f9471b086c5c5782&query=28.6139,77.2090&units=f"
-const longLateUrl = "https://api.geoapify.com/v1/geocode/search?text=Los%20Angeles&apiKey=9d0c2bed5eb24f65b1098ad1c0078218"
-request({ url: url, json: true }, (error, response) => {
-    const data = response.body;
-    const current = data.current
-    if (error) {
-        console.log("erro from error")
+const foreCast = require('./utils/foreCast')
 
-    } else if (data.error) {
-        console.log(data.error)
+const geoCode = require('./utils/geoCode.js')
+if(process.argv.length==2 ){
+    return console.log("please provide address")
+}
+geoCode(process.argv[2], (error, response) => {
+    error ? console.log(error) :
+        foreCast(response.longitude, response.latitude, (error, data) => {
+            error ? console.log(error) :
+                console.log(response.address)
+            console.log("data", data)
 
-    } else {
-        console.log(`currently it is ${current.temperature} hot and there is ${current.precip}% chance of rain`)
-    }
-
-})
-
-request({ url: longLateUrl, json: true }, (error, response) => {
-    const longlateData = response.body.features[0].properties
-    if (error) {
-        return error
-    } else if (longlateData === undefined) {
-        console.log("no data found")
-    } else {
-        console.log(`longitude=${longlateData.lon} and latitude=${longlateData.lat}`)
-    }
-
-    // console.log(longlateData.type)
+        })
 })
