4a7e253baec37df6cb1637011dce19eb39dc9ad1
=Deep.Chhatbar <=deepchhatbar@gmail.com>
2023-08-30 15:44:53 +0530

    added some handlebars and styles

--- webServer/src/app.js
+++ webServer/src/app.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const hbs =require('hbs');
 const express = require('express');
+const { title } = require('process');
 const app = express();
 const pathToPublic = path.join(__dirname, '../public')
-
+const pathToPartial = path.join(__dirname, '../partials')
 
 
 // console.log(__dirname)
@@ -10,22 +12,45 @@ const pathToPublic = path.join(__dirname, '../public')
 app.set('views', path.join(__dirname, '../views'));     
 app.set('view engine', 'hbs')
 app.use(express.static(pathToPublic))
-
+hbs.registerPartials(pathToPartial)
 app.get('', (req, res) => {
     res.render('index',{
-        title:"welcome to index"
+        title:"Weather",
+        footers:"Created by deep"
     })
 })
 app.get('/about',(req,res)=>{
-    res.render("about")
+    res.render("about",{
+        title:"about",
+        footers:"Created by deep",
+
+    })
 })
 app.get('/help',(req,res)=>{
-    res.render("help")
+    res.render("help",{
+        title:"help",
+        footers:"Created by deep",
+    })
 })
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 50,
         location: "ahmedabad"
+    })
+})
+app.get('/help/*',(req,res)=>{
+    res.render("404",{
+        msg:"help title not found",
+        footers:"created by deep",
+        title:"404!"
+
+    })
+})
+app.get('*',(req,res)=>{
+    res.render("404",{
+        msg:" page not found ",
+        footers:"created by deep",
+        title:"404!"
     })
 })
 app.listen(3000, () => {
